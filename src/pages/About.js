1796dac881df8f0ac1c42ab34ce79d6dc385435a
Oliver <oliverahmed000@gmail.com>
2022-06-14 23:39:35 +0600

    About page edited

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,13 +4,7 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
-        dolore ratione, ipsa incidunt velit dolorem! Quaerat numquam harum
-        asperiores. Illum! Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Natus repudiandae non dicta libero tempore? Ipsa vitae quos
-        laborum non illum.
-      </p>
+      <h4>This is a react project where API has used.</h4>
     </section>
   );
 };
